e6cecab2b08c87f577205b6df06b7462b13fcec9
Jimbo <74175693+Ap3x99@users.noreply.github.com>
2023-04-03 22:03:08 -0500

    translate to english

--- Client/Cmain.lua
+++ Client/Cmain.lua
@@ -290,9 +290,9 @@ else
                         if distance < 1.5 then
                             neearSpeaker = k
                             if v.default == false then
-                                QBCore.Functions.HelpNotify("Presiona ~INPUT_CONTEXT~ para acceder a la radio o BackSpace para recogerla")
+                                QBCore.Functions.HelpNotify("Press ~INPUT_CONTEXT~ to access the radio or backspace to pick it up")
                             else
-                                QBCore.Functions.HelpNotify("Presiona ~INPUT_CONTEXT~ para acceder a la radio")
+                                QBCore.Functions.HelpNotify("Press ~INPUT_CONTEXT~ to access the radio")
                             end
                             if IsControlJustPressed(0, 38) then
                                 isNotInRadio = false
